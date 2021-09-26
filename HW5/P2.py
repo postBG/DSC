6700fdd18027ead7e7c8dc8b08c32993c92aefba
postBG <profile2697@gmail.com>
2021-09-27 03:26:11 +0900

    feat: P2 remove newline

--- HW5/P2.py
+++ HW5/P2.py
@@ -1,7 +1,7 @@
 def P2(input_filename: str, output_filename: str) -> None:
     with open(input_filename, 'r') as f:
-        read_contents = [l.strip().split(' ') for l in f.readlines()]
+        read_contents = [l.split(' ') for l in f.readlines()]
 
     with open(output_filename, 'w') as f:
         for name, number, weight in read_contents:
-            f.write(f"{name}'s atomic number is {number} and atomic weight is {weight}\n")
+            f.write(f"{name}'s atomic number is {number} and atomic weight is {weight}")
