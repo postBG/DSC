332637fbb94eb85efbd068a14050bbfc14891056
postBG <profile2697@gmail.com>
2021-10-01 23:38:02 +0900

    fix shit

--- HW3/P6.py
+++ HW3/P6.py
@@ -3,7 +3,7 @@ from typing import List
 
 def P6(alphabet_list: List[str]) -> int:
     for i, e in enumerate(alphabet_list):
-        if 'A' < e < 'a':
+        if 'A' <= e < 'a':
             idx = i
             return idx
     return -1
