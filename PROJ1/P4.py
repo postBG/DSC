8ae74e73c2ad4668a161a5bff3521378ff836844
postBG <profile2697@gmail.com>
2021-10-03 18:10:37 +0900

    done

--- PROJ1/P4.py
+++ PROJ1/P4.py
@@ -1,4 +1,14 @@
-def P4(info: list) -> str:        
-    ##### Write your Code Here #####
+def sex_to_number(sex, year):
+    if 1800 <= year < 1900:
+        return 9 if sex == 'MALE' else 0
 
-    ##### End of your code #####
+    if 1900 <= year < 2000:
+        return 1 if sex == 'MALE' else 2
+
+    if 2000 <= year:
+        return 3 if sex == 'MALE' else 4
+
+
+def P4(info: list) -> str:
+    sex, birth_year, birth_month, birth_day = info
+    return f'{str(birth_year % 100).zfill(2)}{str(birth_month).zfill(2)}{str(birth_day).zfill(2)}{sex_to_number(sex, birth_year)}'
