e7bda231730dbf2483f02ecc2d0c5c8b37bee285
postBG <profile2697@gmail.com>
2021-10-03 16:35:57 +0900

    add p1

--- PROJ1/P1.py
+++ PROJ1/P1.py
@@ -1,4 +1,22 @@
-def P1(game: list) -> int:        
-    ##### Write your Code Here #####
-    
-    ##### End of your code #####
+_BEATER_LOOK_UP = {
+    'R': 'P',
+    'S': 'R',
+    'P': 'S'
+}
+_LOSER_LOOK_UP = {
+    'R': 'S',
+    'S': 'P',
+    'P': 'R'
+}
+
+
+def _B_score(match):
+    a, b, c = match
+    if _BEATER_LOOK_UP[b] in [a, c]:
+        return 0
+
+    return 1 if _LOSER_LOOK_UP[b] in [a, c] else 0
+
+
+def P1(game: list) -> int:
+    return sum([_B_score(match) for match in game])
