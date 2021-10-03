051917ccb642b92761a54eeaffc4843523e0cdd7
postBG <profile2697@gmail.com>
2021-10-03 16:44:44 +0900

    p2

--- PROJ1/P2.py
+++ PROJ1/P2.py
@@ -1,4 +1,9 @@
-def P2(n: int) -> bool:        
-    ##### Write your Code Here #####
-
-    ##### End of your code #####
+def P2(n: int) -> bool:
+    primes = []
+    copied_n = n
+    for denom in range(2, n + 1):
+        while copied_n % denom == 0:
+            copied_n = copied_n // denom
+            primes.append(denom)
+    print(primes)
+    return len(primes) == 2
