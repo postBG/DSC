e220a03f459792e55d04d6c9b9bc66af0be286df
postBG <profile2697@gmail.com>
2021-10-07 02:08:02 +0900

    add p1

--- HW7/P1.py
+++ HW7/P1.py
@@ -1,8 +1,14 @@
+def swap(i, j, num_list):
+    tmp = num_list[i]
+    num_list[i] = num_list[j]
+    num_list[j] = tmp
+
+
 def P1(num_list: list) -> int:
-
-    ### Write code here ###
-
-    
-    ### End of your code ###  
-
-    return
+    cnt = 0
+    for i in range(len(num_list) - 1, 0, -1):
+        for j in range(i):
+            if num_list[j + 1] < num_list[j]:
+                swap(j, j + 1, num_list)
+                cnt += 1
+    return cnt
