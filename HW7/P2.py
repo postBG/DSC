ae4b09defb19de9c88db7410189ab591cf81e6e0
postBG <profile2697@gmail.com>
2021-10-07 02:22:33 +0900

    p2

--- HW7/P2.py
+++ HW7/P2.py
@@ -1,8 +1,15 @@
 def P2(Order: str, S: str) -> str:
+    head = {c: 0 for c in Order}
+    tail = []
+    for c in S:
+        if c in head:
+            head[c] += 1
+        else:
+            tail.append(c)
 
-    ### Write code here ###
+    str1 = []
+    for c, n in head.items():
+        str1.extend([c] * n)
 
-    
-    ### End of your code ###  
-
-    return
+    str1.extend(tail)
+    return ''.join(str1)
