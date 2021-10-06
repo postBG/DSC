9072b552ca3c9d0241738f74c25c211fa02fd217
postBG <profile2697@gmail.com>
2021-10-07 02:32:50 +0900

    p3

--- HW7/P3.py
+++ HW7/P3.py
@@ -1,8 +1,5 @@
 def P3(indexed_s: str) -> str:
-
-    ### Write code here ###
-
-    
-    ### End of your code ###  
-
-    return
+    words = indexed_s.split(' ')
+    words = [(int(w[-1]), w[:-1]) for w in words]
+    sorted_words = [w for i, w in sorted(words)]
+    return ' '.join(sorted_words)
