4855b41edcb1ec9c6ad3a40dabd631943e73f5c3
postBG <profile2697@gmail.com>
2021-10-07 02:48:52 +0900

    P4

--- HW7/P4.py
+++ HW7/P4.py
@@ -1,8 +1,18 @@
+class Str(object):
+    def __init__(self, value):
+        self.value = value
+
+    def __lt__(self, other):
+        if len(self.value) != len(other.value):
+            return len(self.value) < len(other.value)
+
+        return self.value < other.value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+
 def P4(lst: list) -> list:
-
-    ### Write code here ###
-
-    
-    ### End of your code ###  
-
-    return
+    lst = [Str(s) for s in lst]
+    sorted_lst = sorted(lst)
+    return [s.value for s in sorted_lst]
