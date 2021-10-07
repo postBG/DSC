eea8cbdf73c0d4be88a47bda5bda121e7ad464d8
postBG <profile2697@gmail.com>
2021-10-07 13:51:12 +0900

    p3

--- PROJ2/P3.py
+++ PROJ2/P3.py
@@ -5,10 +5,17 @@ Please see instruction document.
 """
 from linked_list_helper import ListNode
 
-def P3(head: ListNode) -> ListNode: 
-    ##### Write your Code Here #####         
 
-    return head
-    ##### End of your code #####
+def P3(head: ListNode) -> ListNode:
+    if head is None:
+        return None
 
+    curr_first = ListNode(head.val)
+    runner = head.next
+    while runner is not None:
+        new_node = ListNode(runner.val)
+        new_node.next = curr_first
+        curr_first = new_node
+        runner = runner.next
 
+    return curr_first
