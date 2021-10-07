b94af0b95ea25ce8773bf9d14bdf3c9b3f2c8f61
postBG <profile2697@gmail.com>
2021-10-07 11:31:03 +0900

    p1

--- PROJ2/P1.py
+++ PROJ2/P1.py
@@ -3,10 +3,42 @@
 Please see instruction document.
 
 """
-def P1(parentheses: str) -> bool:        
-    ##### Write your Code Here #####
+_PAIRS = {
+    ')': '(',
+    '}': '{',
+    ']': '['
+}
 
-    return False
-    ##### End of your code #####
+
+class Stack(object):
+    def __init__(self):
+        super().__init__()
+        self._values = []
+
+    def push(self, value):
+        self._values.append(value)
+
+    def pop(self):
+        popped_value = self._values.pop(-1)
+        return popped_value
+
+    def __len__(self):
+        return len(self._values)
 
 
+def P1(parentheses: str) -> bool:
+    stack = Stack()
+    for c in parentheses:
+        if c in ['(', '{', '[']:
+            stack.push(c)
+            continue
+        if len(stack) == 0:
+            return False
+
+        top = stack.pop()
+        if _PAIRS[c] == top:
+            continue
+        else:
+            return False
+
+    return len(stack) == 0
