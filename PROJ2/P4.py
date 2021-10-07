c7769f08752fd1745e3502f9f88491e8e3083172
postBG <profile2697@gmail.com>
2021-10-07 14:10:46 +0900

    p4

--- PROJ2/P4.py
+++ PROJ2/P4.py
@@ -1,8 +1,34 @@
 from linked_list_helper import ListNode
-def P4(num1: ListNode, num2: ListNode) -> ListNode: 
-    ##### Write your Code Here #####         
-
-    return num1
-    ##### End of your code #####
 
 
+def num_to_lst(num):
+    lst = []
+    curr = num
+    while curr:
+        lst.append(curr % 10)
+        curr = curr // 10
+    return list(reversed(lst))
+
+
+def l2n(lst: ListNode):
+    total = 0
+    runner = lst
+    while runner is not None:
+        total = total * 10 + runner.val
+        runner = runner.next
+    return total
+
+
+def n2l(num: int):
+    curr_head = ListNode(num % 10)
+    curr = num // 10
+    while curr:
+        new_node = ListNode(curr % 10)
+        new_node.next = curr_head
+        curr_head = new_node
+        curr = curr // 10
+    return curr_head
+
+
+def P4(num1: ListNode, num2: ListNode) -> ListNode:
+    return n2l(l2n(num1) + l2n(num2))
