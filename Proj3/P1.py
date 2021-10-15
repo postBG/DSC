562485d461845784742c901a16713a3c22c73a63
postBG <profile2697@gmail.com>
2021-10-15 12:54:50 +0900

    add proj3

---
+++ Proj3/P1.py
@@ -0,0 +1,15 @@
+"""
+**Instruction**
+Please see instruction document.
+
+"""
+from BST_Helper import *
+def P1(root: TreeNode, x:int) -> int:       
+    ##### Write your Code Here #####
+
+
+
+
+
+    return 0
+    ##### End of your code #####
---
+++ Proj3/P2.py
@@ -0,0 +1,18 @@
+"""
+**Instruction**
+Please see instruction document.
+
+"""
+from BST_Helper import *
+from typing import List
+def P2(root: TreeNode) -> List[List[int]]:       
+    ##### Write your Code Here #####
+
+
+
+
+
+    return None
+    ##### End of your code #####
+
+
