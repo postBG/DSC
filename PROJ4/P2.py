88a5e808f0131fc1a2695e7a215d86755203097a
postBG <profile2697@gmail.com>
2021-10-20 02:59:01 +0900

    add p2

--- PROJ4/P2.py
+++ PROJ4/P2.py
@@ -1,5 +1,19 @@
 from typing import List
+
+
+def dfs(graph, start, visited):
+    visited[start] = True
+    for i, f in enumerate(graph[start]):
+        if not visited[f]:
+            dfs(graph, f, visited)
+
+
 def P2(n: int, edges: List[tuple]) -> int:
-    ##### Write your Code Here #####
-    
-    ##### End of your code #####
+    graph = [[] for _ in range(n + 1)]
+    for f1, f2 in edges:
+        graph[f1].append(f2)
+        graph[f2].append(f1)
+
+    visited = [False for _ in range(n + 1)]
+    dfs(graph, 1, visited)
+    return sum(visited)
