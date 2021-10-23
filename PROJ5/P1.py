386aff7fc7d335ea242d5822d67a83683d2f6c6b
postBG <profile2697@gmail.com>
2021-10-23 13:54:40 +0900

    add p1

--- PROJ5/P1.py
+++ PROJ5/P1.py
@@ -1,8 +1,22 @@
+def construct_modular_count_dict(nums, k):
+    count_dict = {i: 0 for i in range(k)}
+    for i in nums:
+        mod = i % k
+        count_dict[mod] += 1
+    return count_dict
+
+
+def check_count_dict(count_dict, k):
+    for i in range(k):
+        if i == 0:
+            if count_dict[i] % 2 != 0:
+                return False
+            continue
+        if count_dict[i] != count_dict[k - i]:
+            return False
+    return True
+
+
 def P1(nums: list, k: int) -> bool:
-
-    ### Write code here ###
-
-    
-    ### End of your code ###  
-
-    return
+    count_dict = construct_modular_count_dict(nums, k)
+    return check_count_dict(count_dict, k)
