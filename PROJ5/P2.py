d253c2f0dec4f68d413dc97f61619c5a5cfab207
postBG <profile2697@gmail.com>
2021-10-23 22:50:56 +0900

    p2

--- PROJ5/P2.py
+++ PROJ5/P2.py
@@ -1,8 +1,11 @@
 def P2(nums: list) -> int:
+    diff_map = {0: [-1]}
+    diff_count = 0
+    for idx, num in enumerate(nums):
+        diff_count += 1 if num == 1 else -1
+        if diff_count not in diff_map:
+            diff_map[diff_count] = []
+        diff_map[diff_count].append(idx)
 
-    ### Write code here ###
-
-    
-    ### End of your code ###  
-
-    return
+    max_len = max([indices[-1] - indices[0] for _, indices in diff_map.items()])
+    return max_len
