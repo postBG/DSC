0fc746d478fbc933ae42eb008bad5fde8eaa2802
postBG <profile2697@gmail.com>
2021-10-24 00:50:15 +0900

    faster ver

--- PROJ5/P4.py
+++ PROJ5/P4.py
@@ -1,15 +1,19 @@
-def series_max_len(num, cache):
-    if cache[num] != -1:
-        return cache[num]
+def series_max_len(num, visited, curr_max_count):
+    if visited[num] == 1:
+        return curr_max_count
 
-    if num + 1 not in cache:
-        cache[num] = 1
-        return cache[num]
-    max_len = series_max_len(num + 1, cache) + 1
-    cache[num] = max_len
-    return cache[num]
+    max_count = 1
+    while num + 1 in visited:
+        visited[num] = 1
+        max_count += 1
+        num += 1
+
+    return max_count if curr_max_count < max_count else curr_max_count
 
 
 def P4(nums: list) -> int:
-    cache = {i: -1 for i in nums}
-    return max([series_max_len(i, cache) for i in nums])
+    visited = {i: 0 for i in nums}
+    max_count = 0
+    for num in nums:
+        max_count = series_max_len(num, visited, max_count)
+    return max_count
