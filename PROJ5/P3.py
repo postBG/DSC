2a4f32875131ed19a6909904ef6b0b3c7b7e65e6
postBG <profile2697@gmail.com>
2021-10-24 00:13:11 +0900

    p3

--- PROJ5/P3.py
+++ PROJ5/P3.py
@@ -1,9 +1,11 @@
 def P3(A: list, B: list) -> int:
+    diff_map = {0: [-1]}
+    diff_count = 0
+    for idx, (a, b) in enumerate(zip(A, B)):
+        diff_count += a - b
+        if diff_count not in diff_map:
+            diff_map[diff_count] = []
+        diff_map[diff_count].append(idx)
 
-    ### Write code here ###
-
-    
-    ### End of your code ###  
-
-    return
-  
+    max_len = max([indices[-1] - indices[0] for _, indices in diff_map.items()])
+    return max_len
