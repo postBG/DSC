672f9b5d2f8c987230d50b0dab4118f561fd73d6
postBG <profile2697@gmail.com>
2021-10-23 15:08:23 +0900

    add p4

--- PROJ5/P4.py
+++ PROJ5/P4.py
@@ -1,10 +1,15 @@
+def series_max_len(num, cache):
+    if cache[num] != -1:
+        return cache[num]
+
+    if num + 1 not in cache:
+        cache[num] = 1
+        return cache[num]
+    max_len = series_max_len(num + 1, cache) + 1
+    cache[num] = max_len
+    return cache[num]
+
+
 def P4(nums: list) -> int:
-
-    ### Write code here ###
-
-    
-    ### End of your code ###  
-
-    return
-    
-    
+    cache = {i: -1 for i in nums}
+    return max([series_max_len(i, cache) for i in nums])
