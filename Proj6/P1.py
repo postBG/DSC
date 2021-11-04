9df9407faf4a1dfd73d3c66ab3940e8df614e70d
postBG <profile2697@gmail.com>
2021-11-04 13:18:18 +0900

    p1

--- Proj6/P1.py
+++ Proj6/P1.py
@@ -1,4 +1,14 @@
-def P1(m: int, n: int) -> int:
-    # write your code below
+def get_bin(num):
+    cnt = 0
+    while num != 1:
+        num //= 2
+        cnt += 1
+    return cnt
 
-    return 
+
+def P1(m: int, n: int) -> int:
+    if m == 0:
+        return 0
+    bm = get_bin(m)
+    bn = get_bin(n)
+    return 1 << bn if bm == bn else 0
