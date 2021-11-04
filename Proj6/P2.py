8b3b95edc2259d7d136b8d1466c7675c9ef49f78
postBG <profile2697@gmail.com>
2021-11-04 14:21:16 +0900

    p2

--- Proj6/P2.py
+++ Proj6/P2.py
@@ -1,5 +1,10 @@
 def P2(n: int) -> bool:
-    # write your code below
+    last_bit = n & 1
 
-
+    while n != 0:
+        n_last_bit = n & 1
+        if last_bit != n_last_bit:
+            return False
+        n = n >> 1
+        last_bit = last_bit ^ 1
     return True
