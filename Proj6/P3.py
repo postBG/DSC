59173dd60f8284f901b0ee7ada3b2ea65cfe7358
postBG <profile2697@gmail.com>
2021-11-06 14:49:26 +0900

    p3

--- Proj6/P3.py
+++ Proj6/P3.py
@@ -1,6 +1,7 @@
-def P3(num:int)->int:
-    # write your code below
-    
-    
-    
-    return 
+def P3(num: int) -> int:
+    ans = 0
+    for _ in range(32):
+        ans = ans << 1
+        ans += num & 1
+        num = num >> 1
+    return ans
