0f1394525c843784302b12b4720d3e5e190a9b7c
postBG <profile2697@gmail.com>
2021-11-25 15:02:10 +0900

    p2

--- HW12/P2.c
+++ HW12/P2.c
@@ -5,15 +5,20 @@ int P2(int n);
 
 // Implement P2 
 // You can define other function, but P2 must return answer.
-int P2(int n){
+int P2(int n) {
+    int sum = 0;
 
+    while (n != 0) {
+        sum += n % 10;
+        n = n / 10;
+    }
 
-    return 0;
+    return sum;
 }
 
 
 // DO NOT MODIFY BELOW!
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
     int ans = P2(n);
 
