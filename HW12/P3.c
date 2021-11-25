6ce109961b865adc99c66438d6b87fa9f78ac9b8
postBG <profile2697@gmail.com>
2021-11-25 15:58:50 +0900

    p3

--- HW12/P3.c
+++ HW12/P3.c
@@ -3,17 +3,30 @@
 
 int P3(int n);
 
+int is_prime(int n) {
+    int i;
+    for (i = 2; (i * i) <= n; i++) {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
 // Implement P3
 // You can define other function, but P3 must return answer.
-int P3(int n){
+int P3(int n) {
+    int sum = 0;
+    int i;
+    for (i = n; i <= 2 * n; i++) {
+        sum += is_prime(i);
+    }
 
-
-    return 0;
+    return sum;
 }
 
 
 // DO NOT MODIFY BELOW!
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
     int ans = P3(n);
 
