1c54ddd91a5ce40c12c14bab483d4f1e92c2aa74
postBG <profile2697@gmail.com>
2021-11-25 16:35:53 +0900

    p2

--- HW13/P2.c
+++ HW13/P2.c
@@ -5,6 +5,27 @@
 
 char *SwitchCase(char *s);
 
+int len(char *s) {
+    int l = 0;
+    while (s[l] != '\0') {
+        l += 1;
+    }
+    return l;
+}
+
+char *SwitchCase(char *s) {
+    int l = len(s);
+    int i;
+    for (i = 0; i < l; i++) {
+        char c = s[i];
+        if ('a' <= c && c <= 'z')
+            s[i] = c - 32;
+        else
+            s[i] = c + 32;
+    }
+    return s;
+}
+
 int main(int argc, char *argv[]) {
     char *str; //List of characters to be sorted
 
@@ -12,11 +33,4 @@ int main(int argc, char *argv[]) {
     printf("%s", SwitchCase(str));
 
     return 0;
-}
-
-/* Do not modify above */
-
-char *SwitchCase(char *s) {
-    /* Write your code below */
-
 }
