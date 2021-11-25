1e933818f4651cf63466634f570a163320d85049
postBG <profile2697@gmail.com>
2021-11-25 14:59:47 +0900

    p1

--- HW12/P1.c
+++ HW12/P1.c
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 
 int P1(int n);
-
-// Implement P1 
+// Implement P1
 // You can define other function, but P1 must return answer.
-int P1(int n){
 
 
-    return 0;
+
+int P1(int n) {
+    if (n == 0) return 0;
+
+    return 2 * P1(n - 1) + 1;
 }
 
 
 // DO NOT MODIFY BELOW!
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
     int ans = P1(n);
 
