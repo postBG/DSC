9dcee077f95d3f0034ea83025526624013af8c60
postBG <profile2697@gmail.com>
2021-12-03 16:09:57 +0900

    p1, p2

--- HW14/P1.c
+++ HW14/P1.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     int N = atoi(argv[1]);
-    // YOUR CODE HERE
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < 2 * i + 1; j++) {
+            printf("*");
+        }
 
+        printf("\n");
+    }
     return 0;
 }
--- HW14/P2.c
+++ HW14/P2.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     char *input_filename = argv[1]; // name of input file
-    // YOUR CODE HERE
-
-
+    FILE *f = fopen(input_filename, "r");
+    int num;
+    int total = 0;
+    while (fscanf(f, "%d", &num) != EOF) {
+        total += num;
+    }
+    printf("%d\n", total);
     return 0;
 }
