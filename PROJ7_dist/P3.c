3a97e7d1838b67ad0c3a01378d90760e5bd92846
postBG <profile2697@gmail.com>
2021-12-09 02:08:21 +0900

    p3

--- PROJ7_dist/P3.c
+++ PROJ7_dist/P3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Linked list node structure */
 struct Node {
@@ -12,6 +13,54 @@ struct Node *createNode(struct Node *newNode, int data) {
     newNode = (struct Node *) malloc(sizeof(struct Node));
     newNode->data = data;
     return newNode;
+}
+
+int find(struct Node *head, int number) {
+    struct Node *curr = head;
+    int index = 0;
+    while (curr != NULL) {
+        if (curr->data == number)
+            return index;
+        curr = curr->next;
+        index++;
+    }
+    return -1;
+}
+
+int list_length(struct Node *head) {
+    struct Node *curr = head;
+    int len = 0;
+    while (curr != NULL) {
+        len++;
+        curr = curr->next;
+    }
+    return len;
+}
+
+int *get_exists(struct Node *head, int *numbers, int num_numbers) {
+    int list_len = list_length(head);
+    int *exists = (int *) malloc(sizeof(int) * list_len);
+    memset(exists, 0, sizeof(int) * list_len);
+
+    for (int i = 0; i < num_numbers; i++) {
+        int index = find(head, numbers[i]);
+        if (index == -1)
+            continue;
+        exists[index] = 1;
+    }
+
+    return exists;
+}
+
+int count_connected(int *exists, int list_len) {
+    int count = 0;
+    for (int i = 0; i < list_len; i++) {
+        if (exists[i] && (i == 0 || exists[i - 1] == 0)) {
+            count++;
+            continue;
+        }
+    }
+    return count;
 }
 
 int main(int argc, char *argv[]) {
@@ -50,19 +99,15 @@ int main(int argc, char *argv[]) {
     fclose(fp);
 
     /* numConnected: the number of connected "cluster" */
-    int numConnected = 0;
 
     /* Write your code here */
-
-
-
-
+    int *exists = get_exists(head, inputArr, length);
+    int numConnected = count_connected(exists, list_length(head));
     /* Do not modify below */
     printf("%d", numConnected);
 
+    free(exists);
     return 0;
-
-
 }
 
 
